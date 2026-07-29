32580b2778afe3155eafcd86c4e8e8b9334dfdfc
Emanuel Onica <37279870+eonica@users.noreply.github.com>
2026-07-30 01:06:59 +0300

    tests counter runAllTests.sh

--- test_scripts/runAllTests.sh
+++ test_scripts/runAllTests.sh
@@ -4,28 +4,28 @@ set -euo pipefail
 echo "Dockerized Tests"
 
 echo "Test#1 100 SCA(UserOps); 25 throttle; 50 rounds;"
-./runTests 100 25 50
+./runTests 100 25 50 12 1
 
 echo "Test#2 100 SCA(UserOps); 50 throttle; 50 rounds; " 
-./runTests 100 50 50 
+./runTests 100 50 50 12 2
 
 echo "Test#3  100 SCA(UserOps); 100 throttle; 50 rounds;"
-./runTests 100 100 50 
+./runTests 100 100 50 12 3
 
 echo "Test#4  75 SCA(UserOps); 25 throttle; 50 rounds;;"
-./runTests 75 25 50 
+./runTests 75 25 50 12 4
 
 echo "Test#5 50 SCA(UserOps); 25 throttle; 50 rounds; "
-./runTests 50 25 50 
+./runTests 50 25 50 12 5
 
 echo "Test#6  25 SCA(UserOps); 25 throttle; 50 rounds;"
-./runTests 25 25 50 
+./runTests 25 25 50 12 6
 
 echo "Test#7 100 SCA(UserOps); 25 throttle; 100 rounds; Block time 6 seconds"
-./runTests 100 25 100 6
+./runTests 100 25 100 6 7
 
 echo "Test#8 100 SCA(UserOps); 25 throttle; 300 rounds; Block time 2 seconds"
-./runTests 100 25 300 2
+./runTests 100 25 300 2 8
 
 echo "Test#9 runIdleTest for idle measurement (no UserOps processing)"
 ./runIdleTest
